c5bf7332e2c579397d32d9a9bf7046cc36c4d8da
shraddha tiwari <113535239+shraddhatiwarii@users.noreply.github.com>
2023-06-23 22:48:31 +0530

    Create Task scheduler.cpp

---
+++ Task scheduler.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int leastInterval(int N, int K, vector<char> &tasks) {
+        // code here
+         vector<int> f(26, 0);
+        
+        for(auto i : tasks)
+            ++f[i - 'A'];
+            
+        int nax = -1;
+        int count = 0;
+        
+        for(int i = 0; i < 26; i++){
+            if(f[i] == nax)
+                ++count;
+            else if(f[i] > nax){
+                nax = f[i];
+                count = 1;
+            }
+        }
+        
+        int ans = max(N, nax + (nax - 1) * K + count - 1);
+        
+        return ans;
+    }
+};
